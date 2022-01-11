7a20a558b840f211aa72edbda5b296f1e2272a5f
williamrolando88 <william.rolando88@hotmail.com>
2022-01-11 15:40:56 -0500

    Revert "Merge branch 'feature/implement-react-router' into dev"
    This reverts commit e0624608d21909d633d72fe019d273b30930e55c.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './components/App';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
-import Quote from './components/Quote';
+import Calculator from './components/Calculator';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Navbar />
-      <div className="h-screen bg-slate-50 flex items-center justify-center">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/calculator" element={<App />} />
-          <Route path="/quote" element={<Quote />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <Calculator />
   </React.StrictMode>,
   document.getElementById('root'),
 );
