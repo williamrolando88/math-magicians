e0624608d21909d633d72fe019d273b30930e55c
williamrolando88 <william.rolando88@hotmail.com>
2022-01-11 15:28:53 -0500

    Merge branch 'feature/implement-react-router' into dev

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Calculator from './components/Calculator';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './components/App';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Quote from './components/Quote';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Calculator />
+    <BrowserRouter>
+      <Navbar />
+      <div className="h-screen bg-slate-50 flex items-center justify-center">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<App />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
