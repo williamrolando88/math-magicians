87fcd70ed25df845fae98e3570b59889ba38bce6
williamrolando88 <william.rolando88@hotmail.com>
2022-01-12 12:06:45 -0500

    Setup for test execution

--- src/__tests__/calculate.test.js
+++
@@ -1,8 +0,0 @@
-import calculate from '../logic/calculate';
-
-calculate;
-test('should calculate', () => {
-  expect(
-    calculate({ total: 123, next: 123, operation: '+' }, 'AC'),
-  ).toStrictEqual({ total: null, next: null, operation: null });
-});
