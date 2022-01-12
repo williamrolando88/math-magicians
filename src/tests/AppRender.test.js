223f2262785fb0d4ddb9e380dd7bbeede47a275d
TheLuisAbarca <arclaav.adm@gmail.com>
2022-01-12 14:51:54 -0500

    Add more tests to App, and separate and test others behaviors.

--- src/tests/AppRender.test.js
+++ src/tests/AppRender.test.js
@@ -3,7 +3,16 @@ import App from '../App';
 import { render, fireEvent } from '@testing-library/react';
 
 describe('Rendering Component: App', () => {
-  test('should render App', () => {
+  test('should render App with default page', () => {
+    const { getByText } = render(<App />);
+    getByText('Math Magicians');
+    getByText('Home');
+    getByText('Calculator');
+    getByText('Quote');
+    getByText('Welcome to our page!');
+  });
+  
+  test('should render Navbar of the App', () => {
     const { getByText } = render(<App />);
     getByText('Math Magicians');
     getByText('Home');
@@ -11,13 +20,25 @@ describe('Rendering Component: App', () => {
     getByText('Quote');
   });
 
-  test('should simulate Home click', () => {
+  test('should simulate navlinks Home click', () => {
     const { getByText } = render(<App />);
     fireEvent.click(getByText('Home'));
     getByText('Welcome to our page!');
   });
 
-  test('should simulate calculation', () => {
+  test('should simulate navlinks Calculator click', () => {
+    const { getByText } = render(<App />);
+    fireEvent.click(getByText('Calculator'));
+    getByText('Give me something to calculate!');
+  });
+
+  test('should simulate navlinks Quote click', () => {
+    const { getByText } = render(<App />);
+    fireEvent.click(getByText('Quote'));
+    getByText('Lets get you inspired!');
+  });
+
+  test('should simulate an operation on calculator', () => {
     const { getByText } = render(<App />);
     fireEvent.click(getByText('Calculator'));
     fireEvent.click(getByText('1'));
